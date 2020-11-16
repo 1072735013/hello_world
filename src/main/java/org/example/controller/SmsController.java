71d065a3cb750b5138d893ec1a47bbaa3f5c23e5
lk <ninglinglikun@163.com>
2020-11-16 16:17:29 +0800

    短信模块第一次修改

---
+++ src/main/java/org/example/controller/SmsController.java
@@ -0,0 +1,18 @@
+package org.example.controller;
+
+public class SmsController {
+
+
+    public static  void say() {
+
+        System.out.println("11111111111111111111111111");
+        System.out.println("11111111111111111111111111");
+        System.out.println("11111111111111111111111111");
+        System.out.println("11111111111111111111111111");
+        System.out.println("11111111111111111111111111");
+        System.out.println("11111111111111111111111111");
+        System.out.println("11111111111111111111111111");
+        System.out.println("11111111111111111111111111");
+
+    }
+}
